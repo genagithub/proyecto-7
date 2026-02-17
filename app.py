fb8d266a664791c35df791f181d13ffb21f1cac1
genagithub <143031972+genagithub@users.noreply.github.com>
2026-02-17 20:15:17 -0300

    commit

--- app.py
+++ app.py
@@ -11,8 +11,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import html, dcc
 from dash.dependencies import Input, Output
 
 
