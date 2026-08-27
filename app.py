46b1916d067cee094fc3efd62001cb4f86d7ff77
genagithub <143031972+genagithub@users.noreply.github.com>
2026-08-27 10:16:44 -0300

    commit

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ app.layout = html.Div(id="body",className="e7_body",children=[
                         clearable=False),
         ]),
         html.Div(id="div_graph_1",className="e7_div_graph",children=[
-            dcc.Graph(id="graph_1",className="e7_graph",figure={})   
+            dcc.Graph(id="graph_1",className="e7_graph",figure={},config={'responsive': True})
         ]),
         html.Div(id="div_graph_2",className="e7_div_graph",children=[
             dcc.Graph(id="graph_2",className="e7_graph",figure={},config={'responsive': True})    
